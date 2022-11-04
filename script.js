3670c6cb936254e08885648e024ca087019d598c
Laurenxx <78702570+awsmPuff@users.noreply.github.com>
2022-11-04 05:20:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -29,10 +29,8 @@ function play() {
         pipes[0].style.height = `${randomHeight}%`;
         pipes[1].style.height = `${100 - (randomHeight + 30)}%`;
         pipes[1].style.top = `${randomHeight + 30}%`;
-    }, 6000);
-    setInterval(() => {
         SCORE += 10;
-    }, 1500);
+    }, 3000);
 }
 
 function collided(source, target) {
